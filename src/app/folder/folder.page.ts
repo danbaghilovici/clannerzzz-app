d4815de25a0abc09b5094d50762abb25339d2735
dntzbgh <dan.baghilovici@gmail.com>
2022-09-19 23:12:54 +0300

    wip UI integration

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -16,63 +16,68 @@ import {File, FileEntry} from '@awesome-cordova-plugins/file/ngx';
 import {Media, MediaObject} from '@awesome-cordova-plugins/media/ngx';
 
 
+// @ts-ignore
+interface AudioFile {
+  // @ts-ignore
+  remoteRef: Reference;
+  localRef?: FileEntry;
+  isDownloading: boolean;
+}
+
+
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  private static FIREBASE_FOLDER_AUDIOS = 'audios';
   public folder: string;
   public fileDir = 'podcast';
+  // controls loading state of UI
+  public loadingAudioFiles: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
-  public files: BehaviorSubject<FileEntry[]> = new BehaviorSubject([]);
+  public availableAudioFiles: BehaviorSubject<AudioFile[]> = new BehaviorSubject([]);
 
 
-  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute, private fbStorage: AngularFireStorage,
-              private platform: Platform, private media: Media, private file: File) {
+  constructor(private http: HttpClient,
+              private activatedRoute: ActivatedRoute,
+              private fbStorage: AngularFireStorage,
+              private platform: Platform,
+              private media: Media,
+              private file: File) {
   }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
     this.platform.ready().then(() => {
-      // this.readFilesDir();
       console.log('platform ready');
-      // const downloadPath = (
-      //   this.platform.is('android')
-      // ) ? this.file.externalDataDirectory : this.file.documentsDirectory;
-      // console.log('download path is', downloadPath + '');
+      this.getAvailableAudios().subscribe(() => {
+        console.log('gata', this.availableAudioFiles.getValue());
+      }, (err) => {
+        console.error('err', err);
+      });
 
-      // console.log(this.fbStorage);
-
-      const ref = this.fbStorage.ref('');
-      // ref.list().subscribe((res) => {
-      //   // @ts-ignore
-      //   const item: Reference = res.items.pop();
-      //   item.getDownloadURL().then(link => {
-      //     console.log(link);
-      //
+      // // @ts-ignore
+      // let items: Reference = null;
+      // // @ts-ignore
+      // ref.list()
+      //   .pipe(switchMap((value, index) => {
+      //     console.log('received list', value);
+      //     // @ts-ignore
+      //     items = value.items.pop();
+      //     return from(items.getDownloadURL());
+      //   }))
+      //   .pipe(switchMap((links) => this.getBlob(links)))
+      //   .pipe(switchMap((b) => this.writeAudioFileCordova(items.name, b)))
+      //   .subscribe(() => {
+      //   }, (err) => {
+      //     console.log('erroare');
+      //     console.error(err);
+      //   }, () => {
+      //     this.readFilesDirCordova();
       //   });
-      //
-      // });
-      // @ts-ignore
-      let items: Reference = null;
-      // @ts-ignore
-      ref.list()
-        .pipe(switchMap((value, index) => {
-          console.log('received list', value);
-          // @ts-ignore
-          items = value.items.pop();
-          return from(items.getDownloadURL());
-        }))
-        .pipe(switchMap((links) => this.getBlob(links)))
-        .pipe(switchMap((b) => this.writeAudioFileCordova(items.name, b)))
-        .subscribe(() => {
-        }, (err) => {
-          console.log('erroare');
-          console.error(err);
-        }, () => {
-          this.readFilesDirCordova();
-        });
     });
 
 
@@ -83,67 +88,25 @@ export class FolderPage implements OnInit {
     try {
       const audio: MediaObject = this.media.create(file.fullPath);
       audio.play();
-    }catch (err){
+    } catch (err) {
       console.error(err);
     }
-    // this.audio2.preloadSimple('audio', file.nativeURL).then(() => {
-    //   this.audio2.play('audio').then((res) => {
-    //     console.log('played ', res);
-    //   }).catch(err => {
-    //     console.error(err);
-    //   });
-    // }).catch(err=>{
-    //   console.error('error on preload', err);
-    // });
-    // this.audio2.preload({assetId: 'audio', assetPath: file.nativeURL, audioChannelNum: 1, isUrl: true})
-    //   .then(() => {
-    //     this.audio2.play({assetId: 'audio'});
-    //   }).catch((err) => {
-    //   console.error(err);
-    // });
+
   }
 
-
-  public readFilesDir(): void {
-    // console.log('reading dir');
-    // this.file.listDir(this.file.externalDataDirectory, '').then((resp: FileEntry[]) => {
-    //   console.log(resp);
-    //   this.files.next(resp);
-    // }).catch(err => {
-    //   console.error(err);
-    // });
-    // Filesystem.readdir({path: '', directory: Directory.Data}).then((res) => {
-    //   console.log('data',res);
-    //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    //   this.files.next([res.files.filter(value => value.name==='test.ogg').pop()]);
-    // });
+  public writeAudioFileCordova(name, datas: Blob): Observable<FileEntry> {
+    return from(this.file.writeFile(this.file.cacheDirectory, name, datas, {replace: true}));
   }
 
-  public async writeAudioFile(name, datas: Blob): Promise<Observable<WriteFileResult>> {
-    console.log('received blob', datas, name);
-    // return from(this.file.writeFile(downloadPath, name, data, {replace: true}));
-
-    return from(Filesystem.writeFile({path: 'test.ogg', data: (await datas.text()), directory: Directory.Data,}));
-  }
-
-  public writeAudioFileCordova(name, datas: Blob): Observable<any> {
-    return from(this.file.writeFile(this.file.cacheDirectory, 'text.ogg', datas, {replace: true}));
-  }
-
-  public readFilesDirCordova(): void {
-    console.log('reading dir');
-    this.file.listDir(this.file.cacheDirectory, '').then((resp: FileEntry[]) => {
-      console.log(resp);
-      this.files.next(resp);
-    }).catch(err => {
-      console.error(err);
-    });
-    // Filesystem.readdir({path: '', directory: Directory.Data}).then((res) => {
-    //   console.log('data',res);
-    //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    //   this.files.next([res.files.filter(value => value.name==='test.ogg').pop()]);
-    // });
-  }
+  // public readFilesDirCordova(): void {
+  //   console.log('reading dir');
+  //   this.file.listDir(this.file.cacheDirectory, '').then((resp: FileEntry[]) => {
+  //     console.log(resp);
+  //     this.availableAudioFiles.next(resp);
+  //   }).catch(err => {
+  //     console.error(err);
+  //   });
+  // }
 
 
   public getBlob(src: any): Observable<Blob> {
@@ -152,5 +115,40 @@ export class FolderPage implements OnInit {
       {
         responseType: 'blob',
       });
+  }
+
+  private getAvailableAudios(): Observable<void> {
+    const fbStorageRef = this.fbStorage.ref(FolderPage.FIREBASE_FOLDER_AUDIOS);
+    // @ts-ignore
+    return fbStorageRef.list().pipe(switchMap((value, index) => {
+      console.log('received list', value);
+      const res: AudioFile[] = [];
+      for (const item of value.items) {
+        console.log(item);
+        const newAudioItem: AudioFile = {remoteRef: item, localRef: null, isDownloading: false};
+        res.push(newAudioItem);
+      }
+      this.availableAudioFiles.next(res);
+      console.log('set data', this.availableAudioFiles.getValue());
+    }));
+  }
+
+  private onDownloadAudioFileClicked(file: AudioFile) {
+    if (!file.remoteRef) {
+      //err
+      return null;
+    }
+    if (file.isDownloading){
+      //err
+      return null;
+    }
+    file.isDownloading = true;
+    this.getBlob(file.remoteRef.getDownloadURL())
+      .pipe(switchMap((value) => this.writeAudioFileCordova(file.remoteRef.name, value)))
+      .subscribe()
+  }
+
+  private setAudioFileLocalSrc(file: AudioFile) {
+
   }
 }
